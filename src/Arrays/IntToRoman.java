c12648c8259e7ec9f808c93788b154109f352427
ialok0 <kumar.alok.787780@gmail.com>
2026-07-05 12:57:00 +0530

    integer to roman convert commit

---
+++ src/Arrays/IntToRoman.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class IntToRoman {
+    public String intToRoman(int num) {
+        String roman = "";
+        int[] values   = {1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] romanLetters = {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+
+        for(int i=0; i<values.length;i++){
+            while(num>=values[i]){
+                roman += romanLetters[i];
+                num -= values[i];
+            }
+        }
+        return roman;
+    }
+}
