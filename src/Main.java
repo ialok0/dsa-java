42fb65a761c9c900ddb7851c65ad270cd4a5c8d7
ialok0 <kumar.alok.787780@gmail.com>
2026-07-18 11:18:26 +0530

    set matrix zeroes commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import Arrays.*;
 import Matrix.RotateMatrix;
+import Matrix.SetMatrixZeroes;
 import Matrix.SpiralMatrix;
 import Strings.*;
 import TwoPointer.ContainerWithMostWater;
@@ -10,6 +11,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
+        int[][] matrix = {{1,1,1},{1,0,1},{1,1,1}};
 
+        SetMatrixZeroes setMatrixZeroes = new SetMatrixZeroes();
+        setMatrixZeroes.setZeroes(matrix);
     }
 }
