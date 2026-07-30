2782a26841e56f9648ec0d83adc9409c73d59304
ialok0 <kumar.alok.787780@gmail.com>
2026-07-30 11:43:02 +0530

    summary ranges commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import HashMap.ContainsDuplicateII;
 import HashMap.GroupAnagram;
 import HashMap.HappyNumber;
 import HashMap.LongestConsecutiveSequence;
+import Intervals.SummaryRanges;
 import Matrix.RotateMatrix;
 import Matrix.SetMatrixZeroes;
 import Matrix.SpiralMatrix;
@@ -15,9 +16,8 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int[] arr = {1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,1,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,80};
-
-        LongestConsecutiveSequence longestConsecutiveSequence = new LongestConsecutiveSequence();
-        System.out.println(longestConsecutiveSequence.longestConsecutive(arr));
+        int[] arr = {0,1,2,4,5,7};
+        SummaryRanges summaryRanges = new SummaryRanges();
+        System.out.println(summaryRanges.summaryRanges(arr));
     }
 }
